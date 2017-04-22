15e01525bcb4dbbbcb77296f86ea78185bb79c9c
Tazeen <tazeenmunnavar33@gmail.com>
2017-04-22 18:14:50 +0530

    Merged find_words and transcribe

--- transcribe.py
+++ transcribe.py
@@ -25,6 +25,7 @@ Example usage:
 import argparse
 import io
 import os
+import re
 # [END import_libraries]
 
 
@@ -68,6 +69,45 @@ def transcribe_gcs(gcs_uri):
 for fn in os.listdir('/home/tazeen/Desktop/speech_python/r'):
      if os.path.isfile(fn):
         transcribe_file(fn)
+
+
+#f1 = open('Inputs/2.txt', 'r')
+c=0
+a=0
+s=0
+o=0
+#import os
+indir = '/home/tazeen/Desktop/speech_python/Inputs'
+for root, dirs, filenames in os.walk(indir):
+    for f in filenames:
+	flag=0
+        f1 = open(os.path.join(root, f), 'r')
+	line_number = 1
+	for line in f1:
+	    for m in re.finditer('call', line):
+		#print "Found", m.group(0), "line", line_number, "at", m.start(), "-", m.end(),f1
+                c+=1
+		flag=1
+		break
+	    for m in re.finditer('ATM', line):
+		#print "Found", m.group(0), "line", line_number, "at", m.start(), "-", m.end(), f1
+		a+=1
+		flag=1
+		break
+	    for m in re.finditer('services', line):
+		#print "Found", m.group(0), "line", line_number, "at", m.start(), "-", m.end(), f1
+		s+=1
+		flag=1
+		break
+	    line_number += 1
+	if flag==0 : o+=1
+	f1.close()
+print "Problem domain\tNumber of customers"
+print "Customer care\t",c
+print "ATM\t\t", a
+print "Services offered at Bank",s
+print "Others\t\t",o
+	
 '''
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
